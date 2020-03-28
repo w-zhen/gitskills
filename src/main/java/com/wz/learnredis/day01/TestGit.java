9c9b88854de568745cdb0ee6126d14db83464a98
w_zhen <423108650@qq.com>
2020-03-29 00:11:35 +0800

    冲突修复

--- src/main/java/com/wz/learnredis/day01/TestGit.java
+++ src/main/java/com/wz/learnredis/day01/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("试验git");
         System.out.println("试验pull，修改一下尼？");
+        System.out.println("冲突处理完了");
     }
 }
