d59a474b641d6f90d165f6e8c5eb7fb6ccf100af
w_zhen <423108650@qq.com>
2020-03-29 13:52:36 +0800

    试验checkout本地分支

--- src/main/java/com/wz/learnredis/day01/sameName.java
+++ src/main/java/com/wz/learnredis/day01/sameName.java
@@ -2,6 +2,6 @@ package com.wz.learnredis.day01;
 
 public class sameName {
     public static void main(String[] args) {
-        String str = "f2-newFile";
+        System.out.println("切换本地分支");
     }
 }
