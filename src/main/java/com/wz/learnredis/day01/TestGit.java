9b9c4371080d5509b744afccbac3d383dab0d2fd
w-zhen <50142846+w-zhen@users.noreply.github.com>
2020-03-29 09:23:35 +0800

    Update TestGit.java

--- src/main/java/com/wz/learnredis/day01/TestGit.java
+++ src/main/java/com/wz/learnredis/day01/TestGit.java
@@ -8,6 +8,7 @@ public class TestGit {
         System.out.println("试验git");
         System.out.println("试验pull，修改一下尼？");
         System.out.println("冲突处理完了");
+        String b="123";
         System.out.println("试验pull按钮");//处理冲突了
         System.out.println("创建分支feature1");//处理冲突了
     }
