60ddc1ab5247c54472249827eec5218599d6090c
w_zhen <423108650@qq.com>
2020-03-29 20:14:47 +0800

    commit三次

--- src/main/java/com/wz/learnredis/day01/sameName.java
+++ src/main/java/com/wz/learnredis/day01/sameName.java
@@ -5,5 +5,7 @@ public class sameName {
         System.out.println("切换本地分支");
         System.out.println("这回试验checkout 远程feature1");
         System.out.println("commit第一次");
+        System.out.println("commit第二次");
+        System.out.println("commit第三次");
     }
 }
