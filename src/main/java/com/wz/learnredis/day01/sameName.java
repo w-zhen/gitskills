7e0cd7255413fcb251c3c46e9ebba3f6fddece91
w_zhen <423108650@qq.com>
2020-03-29 14:43:17 +0800

    试验checkout本地分支

--- src/main/java/com/wz/learnredis/day01/sameName.java
+++ src/main/java/com/wz/learnredis/day01/sameName.java
@@ -3,7 +3,6 @@ package com.wz.learnredis.day01;
 public class sameName {
     public static void main(String[] args) {
         System.out.println("切换本地分支");
-        System.out.println("此时对feature1文件修改，checkout本地feature2文件");
-        System.out.println("对feature2进行修改，checkout本地feature1，会对1有影响吗");
+        System.out.println("这回试验checkout 远程feature1");
     }
 }
