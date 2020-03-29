9750e940a991daeaf65a0058aec4aeb29ccdf3a4
w_zhen <423108650@qq.com>
2020-03-29 19:53:22 +0800

    commit一次

--- src/main/java/com/wz/learnredis/day01/sameName.java
+++ src/main/java/com/wz/learnredis/day01/sameName.java
@@ -3,5 +3,7 @@ package com.wz.learnredis.day01;
 public class sameName {
     public static void main(String[] args) {
         System.out.println("切换本地分支");
+        System.out.println("这回试验checkout 远程feature1");
+        System.out.println("commit第一次");
     }
 }
